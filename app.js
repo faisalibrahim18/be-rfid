a97d25d597305b30873bf255247773f548a28013
faisalibrahim18 <60595722+faisalibrahim18@users.noreply.github.com>
2023-08-07 09:55:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.set('view engine', 'ejs');
 app.use(
     cors({
         credentials: true,
-        origin: "https://lms-rfid.netlify.app",
+        origin: "https://fe-bka.lifetech.co.id",
     }),
 );
 app.use(logger('dev'));
