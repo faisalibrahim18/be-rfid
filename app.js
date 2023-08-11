ab50991ba31a829449be5b2ce7800c970bc1bf92
faisalibrahim18 <60595722+faisalibrahim18@users.noreply.github.com>
2023-08-11 10:02:06 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true'); // Set to 'true'
+
   next();
 });
 
